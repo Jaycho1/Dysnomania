c387223c5a41b6bbf26c2b936b25824ad71f63bf
Janan Aycho <soccerismygame1@hotmail.com>
2017-08-16 14:34:56 -0400

    ReUploaded from Accipiter

---
+++ Dysnomania/Dysnomania/SinglePlay.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Dysnomania
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SinglePlay : ContentPage
+	{
+		public SinglePlay ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
